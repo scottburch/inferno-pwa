144d125134aee5e0519a96f6779748d37de2b7ab
scott <scott@bulldoginfo.com>
2017-02-05 08:54:45 -0800

    added compiling library files

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        'core/helpers/logger.js': './src/core/helpers/logger.js',
+        'core/helpers/request.js': './src/core/helpers/request.js'
+    },
+    output: {
+        filename: 'lib/[name]',
+        libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [/src\/core/],
+                query: {
+                    cacheDirectory: false,
+                    presets: [],
+                    plugins: [
+                        "add-module-exports",
+                        "transform-es2015-modules-commonjs",
+                        "transform-es2015-destructuring",
+                        "transform-object-rest-spread",
+                        "transform-decorators-legacy",
+                        "transform-class-properties",
+                        "inferno",
+                        ["fast-async"]
+                    ]
+                }
+            }
+        ]
+    },
+
+
+    resolve: {
+        alias: {
+            'core': path.resolve('src/core')
+        }
+    },
+
+    plugins: [
+        new webpack.ProvidePlugin({
+            Inferno: 'inferno',
+        })
+    ]
+};
